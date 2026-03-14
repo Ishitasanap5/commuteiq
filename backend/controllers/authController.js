42c64ce5754a35c2fa491be348ba1109e8e320da
Ishita <ishitasanap5@gmail.com>
2026-03-14 11:55:51 +0530

    imlemented user authentication with JWT and mongoDB

---
+++ backend/controllers/authController.js
@@ -0,0 +1,60 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Register User
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(201).json({
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Login User
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(200).json({
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
-import express from 'express';
-import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+
+connectDB();
 
 const app = express();
-
 app.use(cors());
-SuppressedError.use(express.json());
+app.use(express.json());
 
-app.get("/", (req , res)=>{
-    res.send("Commute API running ");
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("CommuteIQ API running");
 });
 
 const PORT = 5000;
-
-app.listen(PORT, ()=>{
-    console.log(`server running pn port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
